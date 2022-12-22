38c3e8b34267f74510fe0dc433163f2fb2a1a082
bulatsabirow <mlbulat@mail.ru>
2022-12-22 23:08:11 +0300

    = convert views.py into module views(#10)

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
 from django.urls import path
 
-from web.views import PlantDetailView
+from web.views.plant import PlantDetailView
 
 urlpatterns = [path("plant/<int:number>", PlantDetailView.as_view(), name="plant")]
