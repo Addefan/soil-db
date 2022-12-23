b47ebf9c7392009941781fd412fba6f9277c3355
lemmehoop <lemmehoop@gmail.com>
2022-12-23 12:32:46 +0300

    ! staff admin

--- web/admin/staff.py
+++ web/admin/staff.py
@@ -2,18 +2,26 @@ from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 
 class StaffAdmin(DjangoUserAdmin):
-
     ordering = ("-last_login",)
-    list_display = ("id", "email", "name", "surname", "last_login", "organization_id")
+    list_display = ("id", "email", "name", "surname", "last_login", "organization_name")
     list_filter = ("last_login",)
+
+    def organization_name(self, obj):
+        return obj.organization.name
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == "organization":
+            formfield.label_from_instance = lambda obj: f'{obj.name}'
+        return formfield
+
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        ("Персональная информация", {"fields": ("name",)}),
+        ("Персональная информация", {"fields": ("name", "surname")}),
         (
             "Доступы",
             {
                 "fields": (
-                    "role",
                     "groups",
                     "user_permissions",
                 ),
