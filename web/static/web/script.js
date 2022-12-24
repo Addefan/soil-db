ce5fcb3746454c5e48dd0df1fab083156a98da5f
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2022-12-24 04:18:26 +0300

    = tooltip moved from template to js (#11)

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    let div_with_email = $("#id_email").parent();
+    div_with_email.attr("data-bs-toggle", "tooltip");
+    div_with_email.attr("data-bs-title", "Почту изменить нельзя");
+    div_with_email.attr("tabindex", 0);
+
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 });
