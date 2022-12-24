547933b575fd7d90bcf97bfe45027c7492433053
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2022-12-25 01:52:40 +0300

    ! receiving data from disabled fields (#12)

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
+    // Setting attributes for the tooltip on the email's input
     let div_with_email = $("#id_email").parent();
     div_with_email.attr("data-bs-toggle", "tooltip");
     div_with_email.attr("data-bs-title", "Почту изменить нельзя");
     div_with_email.attr("tabindex", 0);
 
+    // Activation all tooltips (code from Bootstrap documentation)
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+});
+
+// Removing disable attribute from inputs when form was sent,
+// because data in disabled inputs doesn't send to Django
+$('#profile_form').submit(function () {
+    $("#profile_form :disabled").removeAttr("disabled");
 });
