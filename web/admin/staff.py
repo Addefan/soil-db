57d57a25a97f46f181505b6e477b3c52f1dba2c9
lemmehoop <lemmehoop@gmail.com>
2022-12-25 15:54:13 +0300

    = admin page

--- web/admin/staff.py
+++ web/admin/staff.py
@@ -3,17 +3,8 @@ from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 class StaffAdmin(DjangoUserAdmin):
     ordering = ("-last_login",)
-    list_display = ("id", "email", "name", "surname", "last_login", "organization_name")
+    list_display = ("id", "email", "name", "surname", "last_login", "organization")
     list_filter = ("last_login",)
-
-    def organization_name(self, obj):
-        return obj.organization.name
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
-        if db_field.name == "organization":
-            formfield.label_from_instance = lambda obj: f'{obj.name}'
-        return formfield
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
