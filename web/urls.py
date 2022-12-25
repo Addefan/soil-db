ad3b3f81d389130ed94b9507d649230364182e41
lemmehoop <lemmehoop@gmail.com>
2022-12-25 18:55:32 +0300

    - admin url from web app urls

--- web/urls.py
+++ web/urls.py
@@ -3,8 +3,4 @@ from django.urls import path
 
 from web.view import SoilLoginView, main
 
-urlpatterns = [
-    path("login/", SoilLoginView.as_view(), name="login"),
-    path("admin/", admin.site.urls),
-    path("", main, name="main")
-]
+urlpatterns = [path("login/", SoilLoginView.as_view(), name="login"), path("", main, name="main")]
