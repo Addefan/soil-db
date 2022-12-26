3c1677038344515bed3de8189565e6018dfd0d37
diyanovamir <amirdianov@gmail.com>
2022-12-26 15:28:38 +0300

    = basic url (#18)

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,5 @@ from web.view.add_plant import PlantCreateFormView, ajax_response
 
 urlpatterns = [
     path("plants/add/", PlantCreateFormView.as_view(), name="plants_add"),
-    path("", ajax_response, name="ajax_response"),
+    path("/ajax_response", ajax_response, name="ajax_response"),
 ]
