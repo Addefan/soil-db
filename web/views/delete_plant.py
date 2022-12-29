947df915638735b236e5ab4d6ee124b95fa0bddd
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2022-12-30 02:41:49 +0300

    + view for processing of plant deletion (#28)

---
+++ web/views/delete_plant.py
@@ -0,0 +1,11 @@
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
+
+from web.models import Plant
+
+
+class PlantDeleteView(DeleteView):
+    model = Plant
+    slug_field = "number"
+    slug_url_kwarg = "number"
+    success_url = reverse_lazy("plants")
