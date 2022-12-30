7e0f9605937787bc48916feaacad80543866d264
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2022-12-30 00:35:16 +0000

    = http digit code to enum

--- web/views/profile.py
+++ web/views/profile.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.contrib.auth import update_session_auth_hash
 from django.http import JsonResponse
 from django.urls import reverse_lazy
@@ -28,4 +30,4 @@ class ProfileFormView(UpdateView):
         # Checking if the request wasn't sent via jQuery's Ajax
         if not self.request.headers.get("X-Requested-With") == "XMLHttpRequest":
             return super().form_invalid(form)
-        return JsonResponse(form.errors, status=400)
+        return JsonResponse(form.errors, status=HTTPStatus.NOT_FOUND)
