9b04e0df1b06b444029e23c8ece8d5736cbb0ace
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-01-11 14:08:55 +0000

    ! email immutability tooltip for profile only (#12)

--- web/static/web/profile.js
+++ web/static/web/profile.js
@@ -41,3 +41,15 @@ function save_current_input_values() {
         }
     }
 }
+
+$(document).ready(function () {
+    // Setting attributes for the tooltip on the email's input
+    let div_with_email = $("#id_email").parent();
+    div_with_email.attr("data-bs-toggle", "tooltip");
+    div_with_email.attr("data-bs-title", "Почту изменить нельзя");
+    div_with_email.attr("tabindex", 0);
+
+    // Activation all tooltips (code from Bootstrap documentation)
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+});
--- web/static/web/script.js
+++ web/static/web/script.js
@@ -1,13 +1,2 @@
-$(document).ready(function () {
-    // Setting attributes for the tooltip on the email's input
-    let div_with_email = $("#id_email").parent();
-    div_with_email.attr("data-bs-toggle", "tooltip");
-    div_with_email.attr("data-bs-title", "Почту изменить нельзя");
-    div_with_email.attr("tabindex", 0);
-
-    // Activation all tooltips (code from Bootstrap documentation)
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-});
-
+// Adding a class for all fields that have errors
 $("div .is-invalid").children("input").addClass("is-invalid");
