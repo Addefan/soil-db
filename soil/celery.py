b41e04c8016f67863bc3753a734b3af986563130
diyanovamir <amirdianov@gmail.com>
2023-02-14 22:04:52 +0300

    = variable names

--- soil/celery.py
+++ soil/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notes.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soil.settings")
 
-app = Celery("notes")
+app = Celery("soil")
 app.config_from_object("django.conf:settings", namespace="CELERY")
