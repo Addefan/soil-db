09abc092b4ad1928b05586c3576941715f813566
lemmehoop <lemmehoop@gmail.com>
2023-02-14 21:27:13 +0300

    + transitioning

--- web/transitions/xlsx/transition.py
+++ web/transitions/xlsx/transition.py
@@ -1,12 +1,53 @@
-import datetime
+import os
+import django
 import xlsxwriter
 
+from django.db.models import QuerySet
+from django.utils.datetime_safe import datetime
+from xlsxwriter import Workbook
 
-def queryset_to_workbook():
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soil.settings")
+django.setup()
+
+from web.models import Plant
+
+
+def make_cell_format(wb: Workbook, bold=False, font_color="black", bg_color="white"):
+    cell_format = wb.add_format({"bold": bold, "font_color": font_color, "bg_color": bg_color, "border": True})
+    return cell_format
+
+
+def queryset_to_xlsx(qs: QuerySet):
     workbook = xlsxwriter.Workbook("example.xlsx")
-    worksheet = workbook.add_worksheet()
+    sheet = workbook.add_worksheet("result")
+
+    simple_format = make_cell_format(wb=workbook)
+    header_format = make_cell_format(wb=workbook, bold=True, bg_color="#c8ed72")
+
+    objects = qs.values()
+
+    # writing headers
+    headers = objects.first().keys()
+    for x, header in enumerate(headers):
+        sheet.write_string(0, x, header, cell_format=header_format)
+
+    # writing objects
+    for x, obj in enumerate(objects, start=1):
+        for y, column in enumerate(headers):
+            if type(obj[column]) == str:
+                sheet.write_string(x, y, obj[column], cell_format=simple_format)
+            elif type(obj[column]) == int or type(obj[column]) == float:
+                sheet.write_number(x, y, obj[column], cell_format=simple_format)
+            elif type(obj[column]) == bool:
+                sheet.write_boolean(x, y, obj[column], cell_format=simple_format)
+            elif type(obj[column]) == datetime:
+                sheet.write_datetime(x, y, obj[column], cell_format=simple_format)
+            else:
+                raise TypeError(f"Uncultivited type: {type(obj[column])}")
+
+    sheet.autofit()
     workbook.close()
 
 
 if __name__ == "__main__":
-    queryset_to_workbook()
+    queryset_to_xlsx(Plant.objects.all())
