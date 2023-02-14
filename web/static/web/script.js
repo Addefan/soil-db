4e51a090b729b2f8cb8309be1e203cbdc35a2141
bulatsabirow <mlbulat@mail.ru>
2023-02-15 01:02:54 +0300

    + start making export to xlsx(frontend) #40

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -1,2 +1,7 @@
 // Adding a class for all fields that have errors
 $("div .is-invalid").children("input").addClass("is-invalid");
+
+$('button[data-usage="xlsx-submit"]').on("click", (event) => {
+    let xlsxForm = document.forms.xlsx_form;
+    xlsxForm.submit();
+})
