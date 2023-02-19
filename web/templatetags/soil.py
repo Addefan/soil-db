9c73fde4ca1c61d80638937d3e5f19a80f95c60a
Addefan <vitalyzorin30600000@mail.ru>
2023-02-19 14:06:27 +0300

    + getattr filter in templates

--- web/templatetags/soil.py
+++ web/templatetags/soil.py
@@ -16,16 +16,17 @@ def queryset_names(names_to_select, var):
 def suggestions(stage_form, names_to_select):
     return {
         "stage_form": stage_form,
-        "phylum_title": queryset_names(names_to_select["phylum"], "title"),
-        "phylum_latin_title": queryset_names(names_to_select["phylum"], "latin_title"),
-        "class_title": queryset_names(names_to_select["class"], "title"),
-        "class_latin_title": queryset_names(names_to_select["class"], "latin_title"),
-        "order_title": queryset_names(names_to_select["order"], "title"),
-        "order_latin_title": queryset_names(names_to_select["order"], "latin_title"),
-        "family_title": queryset_names(names_to_select["family"], "title"),
-        "family_latin_title": queryset_names(names_to_select["family"], "latin_title"),
-        "genus_title": queryset_names(names_to_select["genus"], "title"),
-        "genus_latin_title": queryset_names(names_to_select["genus"], "latin_title"),
+        "taxonomy": {
+            "phylum_title": queryset_names(names_to_select["phylum"], "title"),
+            "phylum_latin_title": queryset_names(names_to_select["phylum"], "latin_title"),
+            "class_title": queryset_names(names_to_select["class"], "title"),
+            "class_latin_title": queryset_names(names_to_select["class"], "latin_title"),
+            "order_title": queryset_names(names_to_select["order"], "title"),
+            "order_latin_title": queryset_names(names_to_select["order"], "latin_title"),
+            "family_title": queryset_names(names_to_select["family"], "title"),
+            "family_latin_title": queryset_names(names_to_select["family"], "latin_title"),
+            "genus_title": queryset_names(names_to_select["genus"], "title"),
+            "genus_latin_title": queryset_names(names_to_select["genus"], "latin_title")},
     }
 
 
@@ -33,3 +34,15 @@ def suggestions(stage_form, names_to_select):
 def separate_slice(form, separating_index):
     form, separating_index = list(form), int(separating_index)
     return form[:separating_index], form[separating_index:]
+
+
+@register.filter(name="getattr")
+def get_attr(obj, attr_name):
+    """ Try to get an attribute from an object.
+
+    Example: {% if block|getattr:"editable" %}
+    """
+    try:
+        return obj.get(attr_name, None) or obj.__getattribute__(attr_name)
+    except AttributeError:
+        return None
