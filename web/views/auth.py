8da2ed3c42fe3b9ed569440b525e1ed63b997188
lemmehoop <lemmehoop@gmail.com>
2023-02-21 12:47:05 +0300

    = changed getting message to mixin

--- web/views/auth.py
+++ web/views/auth.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
-from django.shortcuts import redirect
 from django.urls import reverse
 
 from web.forms import AuthForm
@@ -11,12 +11,14 @@ def main(request):
     return HttpResponse(f"{request.user.is_authenticated}")
 
 
-class LoginView(DjangoLoginView):
+class LoginView(SuccessMessageMixin, DjangoLoginView):
     form_class = AuthForm
     template_name = "web/auth.html"
 
+    def get_success_message(self, cleaned_data):
+        return "Вход выполнен успешно!"
+
     def form_valid(self, form):
-        messages.success(self.request, "Вход выполнен успешно!")
         return super(LoginView, self).form_valid(form)
 
     def get_success_url(self):
