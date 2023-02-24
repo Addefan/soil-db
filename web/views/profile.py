ef6c7598cdc3e125db2534279685578686a8809b
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-02-24 13:54:23 +0300

    = sending email instead of changing password (#27)

--- web/views/profile.py
+++ web/views/profile.py
@@ -1,13 +1,20 @@
+import json
+import logging
+from datetime import timedelta
 from http import HTTPStatus
+from uuid import uuid4
 
+import redis
+from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, RedirectView
 
 from web.forms import ProfileForm
 from web.models import Staff
+from web.tasks import send_password_changing_email
 
 
 class ProfileFormView(LoginRequiredMixin, UpdateView):
@@ -20,12 +27,21 @@ class ProfileFormView(LoginRequiredMixin, UpdateView):
         return self.request.user
 
     def form_valid(self, form):
+        user_id = self.request.user.id
+        old_password = Staff.objects.get(pk=user_id).password
+        new_password = form.cleaned_data.pop("password", None)
+        if new_password:
+            form.instance.password = old_password
+            token = uuid4().hex
+            data = {"user_id": user_id, "password": new_password}
+            r = redis.Redis(host="localhost", port=6379, db=0)
+            r.psetex(token, timedelta(hours=1), json.dumps(data))
+            send_password_changing_email.delay(user_id, token)
         response = super().form_valid(form)
-        update_session_auth_hash(self.request, self.request.user)
         # Checking if the request wasn't sent via jQuery's Ajax
         if not self.request.headers.get("X-Requested-With") == "XMLHttpRequest":
             return response
-        return JsonResponse({"success": True})
+        return JsonResponse({"password": bool(new_password)})
 
     def form_invalid(self, form):
         # Checking if the request wasn't sent via jQuery's Ajax
