4d6087ae6e812df59e212efd278a8f692965afc3
diyanovamir <amirdianov@gmail.com>
2023-02-27 15:02:45 +0300

    = modified js

--- web/static/web/plant_crud.js
+++ web/static/web/plant_crud.js
@@ -15,32 +15,43 @@ $('#add_button').on('click', function () {
 function make_custom_attribute(name_attribute, type_attribute, slug_name) {
     console.log(name_attribute, type_attribute)
     let field_form_last_div = $('#plant_form div.input-group').last()
-    let last_field = document.createElement('div')
-    last_field.classList.add('input-group')
-    last_field.classList.add('has-validation')
-    let div_elem = document.createElement('div')
-    div_elem.classList.add('form-floating')
-    div_elem.classList.add('mt-3')
+    let last_field = $('<div>', {
+        class: 'input-group has-validation'
+    })
+    let div_elem = $('<div>', {
+        class: 'form-floating mt-3'
+    })
 
-    let input_field = document.createElement('input')
-    input_field.id = `id_${slug_name}`
-    if (type_attribute === 'string') {
-        input_field.type = 'text'
-    } else if (type_attribute === 'integer') {
-        input_field.type = 'number'
-    } else if (type_attribute === 'float') {
-        input_field.type = 'number'
-        input_field.step = 'any'
-    } else {
-        input_field.type = 'date'
-    }
-    input_field.name = `${slug_name}`
-    input_field.placeholder = 'smt'
-    input_field.classList.add('form-control')
-    let label = document.createElement('label')
-    label.innerHTML = `${name_attribute}`
-    label.htmlFor = `${slug_name}`
-    label.innerHTML = `${name_attribute}`
+    let input_field = $('<input>', {
+        id: `id_${slug_name}`,
+        type: () => {
+            if (type_attribute === 'string') {
+                return 'text'
+            } else if (type_attribute === 'integer') {
+                return 'number'
+            } else if (type_attribute === 'float') {
+                return 'number'
+                // input_field.step = 'any'
+            } else {
+                return 'date'
+            }
+        },
+        name: `${slug_name}`,
+        placeholder: 'smt',
+        class: 'form-control',
+        step: () => {
+            if (type_attribute === 'float'){
+                return 'any'
+            }
+            else {
+                return 'number'
+            }
+        }
+    })
+    let label = $('<label>', {
+        text: `${name_attribute}`,
+        for: `${slug_name}`,
+    })
     div_elem.append(input_field)
     div_elem.append(label)
     last_field.append(div_elem)
@@ -49,6 +60,5 @@ function make_custom_attribute(name_attribute, type_attribute, slug_name) {
 }
 
 $(`#attribute_modal_window`).on('hide.bs.modal', function () {
-    console.log('Я тут')
     $("#attribute_modal_window form")[0].reset()
 })
