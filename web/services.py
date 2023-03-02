6d51b89c24c098dd04305e7f66d7d1ebac17dff6
lemmehoop <lemmehoop@gmail.com>
2023-03-02 21:36:16 +0300

    = method of making str from date

--- web/services.py
+++ web/services.py
@@ -11,11 +11,16 @@ from web.models import Plant
 def create_plant_number() -> int:
     date = now().date()
     last_plant = Plant.objects.filter(digitized_at__gte=date).order_by("-digitized_at").first()
+    number = date.strftime("%y%m%d")
 
     if not last_plant:
-        return int(f"{date.year % 100}{date.month:02}{date.day:02}1")
+        return int(f"{number}1")
 
     serial_number = int(str(last_plant.number)[6:]) + 1
-    number = int(f"{date.year % 100}{date.month:02}{date.day:02}{serial_number}")
+    number = int(f"{number}{serial_number}")
 
     return number
+
+
+if __name__ == "__main__":
+    print(create_plant_number())
