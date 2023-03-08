6b8c286b21fd7940bb885dea6e7d1ccda5607a25
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-03-08 22:40:20 +0300

    = working with the password has been moved to another view (#56)

--- web/views/profile.py
+++ web/views/profile.py
@@ -6,7 +6,10 @@ from uuid import uuid4
 import redis
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from django.http import JsonResponse, Http404
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView, RedirectView
@@ -26,21 +29,11 @@ class ProfileFormView(LoginRequiredMixin, UpdateView):
         return self.request.user
 
     def form_valid(self, form):
-        user_id = self.request.user.id
-        old_password = Staff.objects.get(pk=user_id).password
-        new_password = form.cleaned_data.pop("password", None)
-        if new_password:
-            form.instance.password = old_password
-            token = uuid4().hex
-            data = {"user_id": user_id, "password": new_password}
-            r = redis.Redis(host="localhost", port=6379, db=0)
-            r.psetex(token, timedelta(hours=1), json.dumps(data))
-            send_password_changing_email.delay(user_id, token)
         response = super().form_valid(form)
         # Checking if the request wasn't sent via jQuery's Ajax
         if not self.request.headers.get("X-Requested-With") == "XMLHttpRequest":
             return response
-        return JsonResponse({"password": bool(new_password)})
+        return JsonResponse({"success": True})
 
     def form_invalid(self, form):
         # Checking if the request wasn't sent via jQuery's Ajax
@@ -69,3 +62,19 @@ class ChangePasswordView(RedirectView):
         update_session_auth_hash(self.request, user)
         messages.success(request, "Вы успешно изменили пароль")
         return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        new_password = request.POST.get("password")
+        try:
+            validate_password(new_password)
+        except ValidationError as error:
+            errors = {"password": error.messages}
+            return JsonResponse(errors, status=HTTPStatus.NOT_FOUND)
+        new_password = make_password(new_password)
+        token = uuid4().hex
+        user_id = self.request.user.id
+        data = {"user_id": user_id, "password": new_password}
+        r = redis.Redis(host="localhost", port=6379, db=0)
+        r.psetex(token, timedelta(hours=1), json.dumps(data))
+        send_password_changing_email.delay(user_id, token)
+        return JsonResponse({"success": True})
