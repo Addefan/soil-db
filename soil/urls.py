9b8cb8ad7a4875fdc34e93ca793c0b2929cf354b
bulatsabirow <mlbulat@mail.ru>
2023-03-09 13:57:47 +0300

    + add two temporary urls #55

--- soil/urls.py
+++ soil/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from web.views.errors import Page404View, Page500View
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("web.urls")),
+    # TODO use custom 404 and 500 pages when DEBUG = False
+    path("error404/", Page404View.as_view(), name="page404"),
+    path("error500/", Page500View.as_view(), name="page500"),
 ]
