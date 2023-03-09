993eb5c11098dfbfd4cd86290773a61e9b484079
bulatsabirow <mlbulat@mail.ru>
2023-03-09 13:53:48 +0300

    = refactor errors.py #55

--- web/views/errors.py
+++ web/views/errors.py
@@ -8,19 +8,31 @@ class ErrorPageView(TemplateView):
     # override these attributes
     template_name = None
     status_code = None
+    title = None
+    text = None
+    image = None
 
     def get(self, *args, **kwargs):
         response = render(
-            self.request, self.template_name, {"status_code": self.status_code.value}, status=self.status_code
+            self.request,
+            self.template_name,
+            {"status_code": self.status_code.value, "title": self.title, "text": self.text, "image": self.image},
+            status=self.status_code,
         )
         return response
 
 
 class Page404View(ErrorPageView):
-    template_name = "web/error404.html"
+    template_name = "web/404.html"
     status_code = HTTPStatus.NOT_FOUND
+    title = "Страница не найдена"
+    text = "Возможно, вы неверно указали её адрес или она была перемещена."
+    image = "error404.png"
 
 
 class Page500View(ErrorPageView):
-    template_name = "web/error500.html"
+    template_name = "web/500.html"
     status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+    title = "Внутренняя ошибка сервера"
+    text = "На сервере произошла непредвиденная ошибка." "<br>" "Пожалуйста, подождите: вскоре она будет исправлена."
+    image = "error500.png"
