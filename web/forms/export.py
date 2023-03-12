2e154d06eaffe20795261b8153f03bd5f5654089
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-03-12 21:58:37 +0300

    ! forms after merge

--- web/forms/export.py
+++ web/forms/export.py
@@ -7,4 +7,5 @@ class XlsxColumnsForm(forms.Form):
     columns = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple(attrs={"checked": True}),
         choices=xlsx_columns_choices(),
+        label="Столбцы для экспорта",
     )
