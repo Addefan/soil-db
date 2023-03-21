4122f418ea284dcce1a48337ab9bf3c11a105759
lemmehoop <lemmehoop@gmail.com>
2023-03-21 14:46:18 +0300

    + plants table in vue

--- web/static/web/frontend_vue/src/App.vue
+++ web/static/web/frontend_vue/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
   import FiltersPanel from "@/components/filtersPanel.vue";
+  import PlantsTable from "@/components/plantsTable.vue";
 </script>
 
 <template>
-  <filters-panel></filters-panel>
+  <div class="row">
+    <div class="col mb-3 mt-3 pe-1">
+        <filters-panel></filters-panel>
+    </div>
+    <div class="col-10 mb-3 mt-3">
+        <plants-table></plants-table>
+    </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ web/static/web/frontend_vue/src/components/plantsTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex-wrapper table-color m-0 rounded-4">
+    <table class="table table-hover table-bordered vh-100 border-light">
+      <thead class="thead-dark rounded-4">
+      <tr class="content-align">
+        <th scope="col">Уникальный номер</th>
+        <th scope="col">Наименование растения</th>
+        <th scope="col">Латинское наименование растения</th>
+        <th scope="col">Род</th>
+        <th scope="col">Организация</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr class="content-align" v-for="plant in this.$store.state.plants" :key="plant">
+          <td><a href="" style="color: #000">{{ plant.number }}</a></td>
+          <td>{{ plant.name }}</td>
+          <td>{{ plant.latin_name }}</td>
+          <td>{{ plant.genus }}</td>
+          <td>{{ plant.organization }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "plantsTable"
+}
+</script>
+
+<style scoped>
+
+</style>
