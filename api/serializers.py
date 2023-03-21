a2f4cfb7e0c524f6aa8ad00b5931db7ccbb01e68
bulatsabirow <mlbulat@mail.ru>
2023-03-21 23:50:05 +0300

    ! unused code #65

--- api/serializers.py
+++ api/serializers.py
@@ -3,25 +3,7 @@ from rest_framework import serializers
 from web.models import Plant
 
 
-class PlantSerializerMixin:
-    def fill_in_taxon_fields(self, taxa):
-        for taxon in taxa:
-            self.instance[self.translation[taxon.level]] = taxon.title
-            self.instance[f"{self.translation[taxon.level]} (лат.)"] = taxon.latin_title
-
-    def fill_in_eav_fields(self, eav_fields):
-        for field in eav_fields:
-            self.instance[field.attribute.name] = field.value
-
-    def fill_in_missing_fields(self, attributes):
-        # fill in all missing fields as None
-        for attr in attributes:
-            self.instance.setdefault(attr.name, None)
-
-
-class PlantSerializer(serializers.ModelSerializer, PlantSerializerMixin):
-    # 'translation' attribute isn't used in serializing
-    translation = Plant._taxa
+class PlantSerializer(serializers.ModelSerializer):
     organization = serializers.StringRelatedField()
 
     class Meta:
