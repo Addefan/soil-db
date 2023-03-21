5bbbeaf5aa6ffb397ca97c4d01f5852eabd7dcd8
bulatsabirow <mlbulat@mail.ru>
2023-03-21 15:44:20 +0300

    + serializer includes eav fields #65

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,36 @@
+from eav.models import Entity, Value
 from rest_framework import serializers
 
 from web.models import Plant
 
 
+# class EntitySerializer(serializers.ModelSerializer):
+#     eav_fields = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = Entity
+#         fields = ['id', 'name', 'eav_fields']
+#
+#     def get_eav_fields(self, obj):
+#         request = self.context['request']
+#         eav_data = {}
+#         values = Value.objects.filter(entity=obj)
+#         for value in values:
+#             if value.attribute.entity_type == request.query_params.get('entity_type'):
+#                 attribute_name = value.attribute.name
+#                 eav_data[attribute_name] = value.value
+#         return eav_data
+
+
 class PlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
-        exclude = ("genus",)
+        fields = ("number", "digitized_at", "latin_name", "name", "organization", "genus")
+
+    def to_representation(self, instance):
+        instance = super(PlantSerializer, self).to_representation(instance)
+        plants = self.context.get("plants")
+        eav_fields = self.context.get("eav_fields")
+        for field in eav_fields:
+            instance.setdefault(field.attribute.name, field.value)
+        return instance
--- api/views.py
+++ api/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from eav.models import Value
 from rest_framework import generics
+from rest_framework.response import Response
 
 from api.serializers import PlantSerializer
 from web.models import Plant
 
 
-class PlantAPIView(generics.ListAPIView):
+class PlantAPIView(generics.GenericAPIView):
     serializer_class = PlantSerializer
-    queryset = Plant.objects.all()
+    queryset = Plant.objects.prefetch_related("organization")
+
+    def get(self, request, *args, **kwargs):
+        plants = self.get_queryset()
+        plants_id = [plant.id for plant in plants]
+        eav_fields = Value.objects.filter(entity_id__in=plants_id).prefetch_related("attribute")
+        serializer = PlantSerializer(plants, many=True, context={"plants": plants, "eav_fields": eav_fields})
+        return Response(serializer.data)
