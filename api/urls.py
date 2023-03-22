c09ffb2267b661fe6ccd66aecb8ad486dca70dd4
bulatsabirow <mlbulat@mail.ru>
2023-03-22 17:23:15 +0300

    ! rename api url path #65

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from api.views import PlantAPIView
 
-urlpatterns = [path("get/", PlantAPIView.as_view(), name="get_plants")]
+urlpatterns = [path("plants/", PlantAPIView.as_view(), name="get_plants")]
