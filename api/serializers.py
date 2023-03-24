a909c4c3c3758036185b29f37e9ce67fddd46089
diyanovamir <amirdianov@gmail.com>
2023-03-24 10:14:03 +0300

    = returning variable

--- api/serializers.py
+++ api/serializers.py
@@ -15,4 +15,4 @@ class PlantSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         instance = super(PlantSerializer, self).to_representation(instance)
-        return self.context.get("data") if instance["number"] in self.context.get("data").keys() else {}
+        return self.context["data"].get(instance["number"], {})
