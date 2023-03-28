4ffcb9836dda412ae60b265bebdf3074a823802b
bulatsabirow <mlbulat@mail.ru>
2023-03-28 12:40:48 +0300

    + CustomDateFilter component #76

---
+++ web/static/web/frontend_vue/src/components/CustomDateFilter.vue
@@ -0,0 +1,16 @@
+<template>
+  <VueDatePicker range locale="ru" cancelText="Закрыть" selectText="Выбрать" :enable-time-picker="false"></VueDatePicker>
+</template>
+
+<script>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+export default {
+  components: {VueDatePicker},
+  name: "CustomDateFilter"
+}
+</script>
+
+<style scoped>
+
+</style>
