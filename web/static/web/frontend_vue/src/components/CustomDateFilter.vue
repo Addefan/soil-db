4086457b0c9d9ba27f8d697a4ad14233ceb97072
bulatsabirow <mlbulat@mail.ru>
2023-03-28 13:53:09 +0300

    + props for CustomDateFilter component #76

--- web/static/web/frontend_vue/src/components/CustomDateFilter.vue
+++ web/static/web/frontend_vue/src/components/CustomDateFilter.vue
@@ -1,11 +1,17 @@
 <template>
-  <VueDatePicker range locale="ru" cancelText="Закрыть" selectText="Выбрать" :enable-time-picker="false"></VueDatePicker>
+  <VueDatePicker :date="date" range locale="ru" cancelText="Закрыть" selectText="Выбрать"></VueDatePicker>
 </template>
 
 <script>
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 export default {
+  props: {
+    date: {
+      type: Date,
+      required: false,
+    }
+  },
   components: {VueDatePicker},
   name: "CustomDateFilter"
 }
