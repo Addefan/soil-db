c6b10867997dd970c443eeaa834fbd97e647bc3c
lemmehoop <lemmehoop@gmail.com>
2023-03-29 09:43:40 +0300

    ! slider's inputs work logic

--- web/static/web/frontend_vue/src/components/NumberInput.vue
+++ web/static/web/frontend_vue/src/components/NumberInput.vue
@@ -4,14 +4,14 @@
       <slider v-model="value" :min="this.min" :max="this.max" />
     </div>
     <div class="row">
-      <div class="col-6">
-      <div class="input-group mb-3">
+      <div class="col-6 px-2">
+      <div class="input-group input-group-sm mb-3">
         <span class="input-group-text px-1" id="from">От</span>
         <input type="number" v-model="value[0]" class="form-control px-1" aria-describedby="from">
       </div>
       </div>
-      <div class="col-6">
-      <div class="col-6 input-group  mb-3">
+      <div class="col-6 px-2">
+      <div class="col-6 input-group input-group-sm mb-3">
         <span class="input-group-text px-1" id="to">До</span>
         <input type="number" v-model="value[1]" class="form-control px-1" aria-describedby="to">
       </div>
@@ -42,9 +42,18 @@ export default {
       handler(new_val) {
         if (new_val[0] < this.min) {
           this.value[0] = this.min;
+        } else if (new_val[0] > this.max) {
+          this.value[0] = this.max;
         }
+
         if (new_val[1] > this.max) {
           this.value[1] = this.max;
+        } else if (new_val[1] < this.min) {
+          this.value[1] = this.min;
+        }
+
+        if (new_val[0] > new_val[1]) {
+          this.value[0] = this.value[1];
         }
       },
       deep: true
