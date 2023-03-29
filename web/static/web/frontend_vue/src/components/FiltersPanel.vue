8919764cafa29b0a8afe2cfc6b72a686278c5c69
diyanovamir <amirdianov@gmail.com>
2023-03-29 15:42:15 +0300

    + SearchSelect component

--- web/static/web/frontend_vue/src/components/FiltersPanel.vue
+++ web/static/web/frontend_vue/src/components/FiltersPanel.vue
@@ -1,24 +1,47 @@
 <template>
   <div class="rounded-4 h-100 mh-50 table-color">
-    <div v-for="param in this.getParameters()" :key="param" class="pt-1 px-2">
-      {{ param }}
-      <!--TODO: component depending on parameter type-->
-    </div>
-    <div class="text-center buttons position-sticky top-100 mb-1">
-      <button type="button" class="btn btn-success btn-sm me-1" data-bs-toggle="modal" data-bs-target="#ModalXlsx">Применить</button>
-      <button type="button" class="btn btn-outline-success ms-1 btn-sm mx-auto export_to_xlsx__button" data-bs-toggle="modal" data-bs-target="#ModalXlsx">Экспорт</button>
-    </div>
+    <form @submit.prevent="submitEvent">
+      <div v-for="param in this.getParameters()" :key="param" class="pt-1 px-2">
+        {{ param.russian_name }}
+        <div v-if="param.type === 'text'">
+          <SearchSelect @change="collectAttrib" :variants="param.values"></SearchSelect>
+        </div>
+        <!--TODO: component depending on parameter type-->
+      </div>
+      <div class="text-center buttons position-sticky top-100 mb-1">
+        <button class="btn btn-success btn-sm me-1">
+          Применить
+        </button>
+        <button type="button" class="btn btn-outline-success ms-1 btn-sm mx-auto export_to_xlsx__button"
+                data-bs-toggle="modal" data-bs-target="#ModalXlsx">Экспорт
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 import {mapActions, mapGetters} from "vuex";
+import SearchSelect from "@/components/SearchSelect.vue";
+import { toRaw } from 'vue';
 
 export default {
   name: "FiltersPanel",
+  data(){
+    return {
+      values: []
+    }
+  },
+  components: {SearchSelect},
   methods: {
     ...mapActions(["loadParameters"]),
     ...mapGetters(["getParameters"]),
+    collectAttrib(v){
+      this.values.push(v)
+    },
+    submitEvent(){
+      console.log(toRaw(this.values))
+    }
   },
   mounted() {
     this.loadParameters();
