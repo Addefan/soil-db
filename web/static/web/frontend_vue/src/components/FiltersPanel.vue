d866b479379969844dd99f7ece8c9a5d43092064
lemmehoop <lemmehoop@gmail.com>
2023-04-04 12:36:39 +0300

    ! filters panel background color work logic

--- web/static/web/frontend_vue/src/components/FiltersPanel.vue
+++ web/static/web/frontend_vue/src/components/FiltersPanel.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="rounded-4 h-100 mh-50 table-color">
-    <form class="pt-2" @submit.prevent="this.loadPlants">
-      <div v-for="param in this.getAttributes()" :key="param" class="mb-2">
-        <div class="px-2">{{ param.russian_name }}</div>
-        <div v-if="param.type === 'text'">
-          <SearchSelect :variants="param.values" class="px-2"
-                        @change="(data) => handleFilter(param.english_name, data)"></SearchSelect>
+  <div class="h-100 mh-50">
+    <div class="table-color rounded-4">
+      <form class="pt-2" @submit.prevent="this.loadPlants">
+        <div v-for="param in this.getAttributes()" :key="param" class="mb-2">
+          <div class="px-2">{{ param.russian_name }}</div>
+          <div v-if="param.type === 'text'">
+            <SearchSelect :variants="param.values" class="px-2"
+                          @change="(data) => handleFilter(param.english_name, data)"></SearchSelect>
+          </div>
+          <div v-else-if="param.type === 'date'">
+            <CustomDateFilter class="px-2"
+                @change="(data) => handleFilter(param.english_name, data)"></CustomDateFilter>
+          </div>
+          <number-input v-else-if="param.type === 'float' || param.type === 'int'" :min="param.values[0]"
+                        :max="param.values[1]" @change="(data) => handleFilter(param.english_name, data)"
+                        class="py-2 px-3"/>
         </div>
-        <div v-else-if="param.type === 'date'">
-          <CustomDateFilter class="px-2"
-              @change="(data) => handleFilter(param.english_name, data)"></CustomDateFilter>
+        <div class="text-center buttons position-sticky top-100 mb-1">
+          <button class="btn btn-success btn-sm me-1">
+            Применить
+          </button>
+          <button type="button" class="btn btn-outline-success ms-1 btn-sm mx-auto export_to_xlsx__button"
+                  data-bs-toggle="modal" data-bs-target="#ModalXlsx">Экспорт
+          </button>
         </div>
-        <number-input v-else-if="param.type === 'float' || param.type === 'int'" :min="param.values[0]"
-                      :max="param.values[1]" @change="(data) => handleFilter(param.english_name, data)"
-                      class="py-2 px-3"/>
-      </div>
-      <div class="text-center buttons position-sticky top-100 mb-1">
-        <button class="btn btn-success btn-sm me-1">
-          Применить
-        </button>
-        <button type="button" class="btn btn-outline-success ms-1 btn-sm mx-auto export_to_xlsx__button"
-                data-bs-toggle="modal" data-bs-target="#ModalXlsx">Экспорт
-        </button>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </template>
 
