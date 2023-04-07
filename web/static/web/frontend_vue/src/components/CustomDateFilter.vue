7222c6a1932add601e288a1762999a301041d6c1
bulatsabirow <mlbulat@mail.ru>
2023-04-07 21:08:03 +0300

    = reformat vue datepicker input feature/63

--- web/static/web/frontend_vue/src/components/CustomDateFilter.vue
+++ web/static/web/frontend_vue/src/components/CustomDateFilter.vue
@@ -1,6 +1,9 @@
 <template>
   <VueDatePicker v-model="date" range locale="ru" cancelText="Закрыть" selectText="Выбрать"
-                 :enable-time-picker="false" @update:model-value="$emit('change', date ?? [])"></VueDatePicker>
+                 :enable-time-picker="false" :preview-format="previewFormat"
+                 partial-range :partial-range="false" placeholder="Pick some"
+                 :format="format" @update:model-value="$emit('change', date ?? [])">
+  </VueDatePicker>
 </template>
 
 <script>
@@ -8,8 +11,24 @@ import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 export default {
   data() {
+    const convertDateComponent = (component) => {
+      return +component < 10 ? `0${component}` : component;
+    }
+    const beautifyDate = (date) => {
+      const startDay = convertDateComponent(date.getDate());
+      const startMonth = convertDateComponent(date.getMonth() + 1);
+      const startYear = convertDateComponent(date.getFullYear());
+      return `${startDay}.${startMonth}.${startYear}`
+    }
     return {
       date: [],
+      format(date) {
+        date = date.map(elem => beautifyDate(elem));
+        return date.join(" — ");
+      },
+      previewFormat(date) {
+        return date.map(elem => beautifyDate(elem));
+      }
     }
   },
   components: {VueDatePicker},
