32283389435b9f0f25dbfb3781c699cd99a2b2a9
diyanovamir <amirdianov@gmail.com>
2023-05-21 23:56:21 +0300

    - debug prints

--- web/services/password.py
+++ web/services/password.py
@@ -14,8 +14,7 @@ def create_password_change_request(request):
     origin = build_origin_from_request(request)
 
     new_password = make_password(new_password)
-    password_change = PasswordChange(password=new_password, user_id=user_id)
-    password_change.save()
+    password_change = PasswordChange.objects.create(password=new_password, user_id=user_id)
     token = password_change.id
 
     send_password_changing_email.delay(origin, user_id, token)
@@ -24,24 +23,14 @@ def create_password_change_request(request):
 
 def process_password_change_request(request):
     token = request.GET.get("token")
-    print(token)
     if token is None:
-        print("Первое")
         return False
 
     password_change = PasswordChange.objects.filter(pk=token).first()
     user_id = request.user.id
-    print(user_id)
-    print(password_change)
+
     # checking whether the link has expired
     if password_change is None:
-        print("Второе")
-        return False
-
-    # checking whether who tried to change the password and who clicked the link is the same user
-    print(password_change.user_id)
-    if password_change.user_id != user_id:
-        print("Третье")
         return False
 
     user = Staff.objects.get(pk=user_id)
