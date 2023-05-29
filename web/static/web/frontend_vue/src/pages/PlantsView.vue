ea1139cd95559e9b1943e295f9f8019c327339ba
Vitaly Zorin <vitalyzorin30600000@mail.ru>
2023-05-29 17:37:02 +0300

    ! sidebar and footer

--- web/static/web/frontend_vue/src/pages/PlantsView.vue
+++ web/static/web/frontend_vue/src/pages/PlantsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col mb-3 mt-3 pe-1 ms-1">
+    <div class="col-2 mb-3 mt-3 pe-1">
         <filters-panel></filters-panel>
     </div>
     <div class="col-10 mb-3 mt-3">
